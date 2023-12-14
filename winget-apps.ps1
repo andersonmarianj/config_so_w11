668d2a481deb678bcd770ae871dbfb44cb9bbfa6
Anderson Mariano <92185295+andersonmarianj@users.noreply.github.com>
2023-12-13 23:15:31 -0300

    Add files via upload

---
+++ winget-apps.ps1
@@ -0,0 +1,18 @@
+# spotify
+winget install -e --id Spotify.Spotify; 
+# discord
+winget install -e --id Discord.Discord; 
+# vscode 
+winget install -e --id Microsoft.VisualStudioCode;
+# node
+winget install -e --id OpenJS.NodeJS; 
+# dbeaver
+winget install -e --id dbeaver.dbeaver;
+# docker
+winget install -e --id Docker.DockerDesktop;
+# git
+winget install -e --id Git.Git
+# insomnia
+winget install -e --id Insomnia.Insomnia
+# postman
+winget install -e --id Postman.Postman
